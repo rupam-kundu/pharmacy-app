bdf886fde740472d0f98fe6719505be6dcc36abf
Rupam Kundu <rupamkundu1000000@gmail.com>
2022-06-25 18:46:40 +0530

    Changed theme.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
+import { ThemeProvider, Theme } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import awsExports from "./aws-exports";
 import Layout from "./components/Layout";
@@ -10,22 +11,39 @@ import EditMed from "./components/EditMedicine";
 
 Amplify.configure(awsExports);
 
+const theme = {
+  name: "my-theme",
+  tokens: {
+    colors: {
+      brand: {
+        primary: {
+          80: {
+            value: "#3B2F4F",
+          },
+        },
+      },
+    },
+  },
+};
+
 function App() {
   return (
-    <Authenticator>
-      {({ signOut }) => (
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout signOut={signOut} />}>
-              <Route index element={<MedicinesList />} />
-              <Route path="list" element={<MedicinesList />} />
-              <Route path="add" element={<AddMed />} />
-              <Route path="edit" element={<EditMed />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      )}
-    </Authenticator>
+    <ThemeProvider theme={theme}>
+      <Authenticator>
+        {({ signOut }) => (
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Layout signOut={signOut} />}>
+                <Route index element={<MedicinesList />} />
+                <Route path="list" element={<MedicinesList />} />
+                <Route path="add" element={<AddMed />} />
+                <Route path="edit" element={<EditMed />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        )}
+      </Authenticator>
+    </ThemeProvider>
   );
 }
 
